d5e6edda635d7d9b023687cb172925bf6eae8170
jorge aguilar <jorge.aguilar@ceiba.com.co>
2021-10-06 16:34:56 -0500

    +1

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ http.createServer(async function (req, res) {
 
         response.on('end', () => {
             res.setHeader('Content-Type', 'application/json');
-            res.write(data.replace('}', ', "test": "nuevo1" }'));
+            res.write(data.replace('}', ', "test": "nuevo2" }'));
             res.end();
         });
     })
