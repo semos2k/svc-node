1204c96d3aba4db488ee2a15240508b370e8d6f0
jorge aguilar <jorge.aguilar@ceiba.com.co>
2021-10-11 15:17:01 -0500

    prueba con rust y multi stage

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
-FROM node:13.7.0-alpine3.10
+FROM rust:1.55.0-alpine3.13 as builder
 
-#=======================
-# General Configuration
-#=======================
-EXPOSE 5000
+RUN apk add musl-dev && \
+    rm -rf /var/lib/apt/lists/* && \
+    rm /var/cache/apk/*
 
-RUN mkdir -p /app
-WORKDIR /app
+WORKDIR /usr/src/app
+RUN USER=root cargo init
+COPY Cargo.toml .
+COPY src src
+RUN cargo build --release
 
-COPY index.js /app/
-
-CMD ["node", "/app/index.js"]
+#FROM debian:stretch-slim
+FROM alpine:3.13.6
+COPY --from=builder /usr/src/app/target/release/api-rust /bin/
+CMD api-rust
---
+++ src/main.rs
@@ -0,0 +1,56 @@
+#![deny(warnings)]
+
+use std::env::var;
+use hyper::client::HttpConnector;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Client, Request, Response, Server};
+
+type GenericError = Box<dyn std::error::Error + Send + Sync>;
+type Result<T> = std::result::Result<T, GenericError>;
+
+//static URL: &str = "http://127.0.0.1:3000";
+
+async fn hello(mut _req: Request<Body>, client: Client<HttpConnector>) 
+        -> Result<Response<Body>> {
+    let api_hostname = var("SVC_API_HOSTNAME").unwrap_or("localhost".to_string());
+    let api_port = var("SVC_API_PORT").unwrap_or("4000".to_string());
+
+    let uri_string = format!(
+        "http://{}:{}{}",
+        api_hostname, api_port,
+        _req.uri()
+            .path_and_query()
+            .unwrap()
+    );
+    let uri = uri_string.parse().unwrap();
+    *_req.uri_mut() = uri;
+    let web_res = client.request(_req).await?;
+
+    Ok(Response::new(web_res.into_body()))
+}
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+    //pretty_env_logger::init();
+    let client = Client::new();
+
+    let make_svc = make_service_fn(move |_| {
+        let client = client.clone();
+        async {
+            Ok::<_, GenericError>(service_fn(move |req| {
+                hello(req, client.to_owned())
+            }))
+        }
+    });
+
+    let addr = ([127, 0, 0, 1], 5000).into();
+    let server = Server::bind(&addr).serve(make_svc);
+    let api_hostname = var("SVC_API_HOSTNAME").unwrap_or("localhost".to_string());
+    let api_port = var("SVC_API_PORT").unwrap_or("4000".to_string());
+
+    println!("Listening on http://{}", addr);
+    println!(" - consumer endpoint: http://{}:{}", api_hostname, api_port);
+    server.await?;
+
+    Ok(())
+}
