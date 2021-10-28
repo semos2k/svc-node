0ae31e14715147a16c5faf3e04bfff7892aa78ae
Jose A <jose.fernandez@ceiba.com.co>
2021-10-28 16:24:11 -0500

    fixed port

--- Program.cs
+++ Program.cs
@@ -1,15 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddHttpClient("client", cfg => {
     var hostName = Environment.GetEnvironmentVariable("SVC_API_HOSTNAME");
     var port = Environment.GetEnvironmentVariable("SVC_API_PORT");
     cfg.BaseAddress = new Uri($"http://{hostName}:{port}");
 });
+
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-
+Environment.SetEnvironmentVariable("ASPNETCORE_URLS", "http://localhost:5000");
 
 app.MapGet("/", async (IHttpClientFactory clientFactory, HttpContext context) => {
     var query = context.Request.Query["number"];
@@ -17,6 +16,8 @@ app.MapGet("/", async (IHttpClientFactory clientFactory, HttpContext context) =>
     var response = await client.GetAsync($"/?number={query}");
     return await response.Content.ReadAsStringAsync();   
 });
+
+app.MapGet("/health", () => "Ok");
 
 
 app.Run();
